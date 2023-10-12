d1100661caf9cb41f5f000b4439a76af21a488c5
ahoang2502 <athoang252@gmail.com>
2023-10-12 15:10:30 +1100

    user db

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,23 @@
+import { db } from "@/lib/db";
+
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
 	const { getUser } = getKindeServerSession();
 	const user = getUser();
 
 	if (!user || !user.id) return redirect("/auth-callback?origin=dashboard");
 
-	
+	const dbUser = await db.user.findFirst({
+		where: {
+			id: user.id,
+		},
+	});
 
-	return <div>DashboardPage</div>;
+	if (!dbUser) return redirect("/auth-callback?origin=dashboard");
+
+	return <Dashboard />;
 };
 
 export default DashboardPage;
--- components/Providers.tsx
+++ components/Providers.tsx
@@ -12,7 +12,7 @@ const Providers = ({ children }: PropsWithChildren) => {
 		trpc.createClient({
 			links: [
 				httpBatchLink({
-					url: "hhtp://localhost:3000/api/trpc",
+					url: "http://localhost:3000/api/trpc",
 				}),
 			],
 		})
