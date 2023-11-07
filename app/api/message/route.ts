349b793daa4a2a5666c0981db067cee0f3787314
ahoang2502 <athoang252@gmail.com>
2023-11-07 19:48:22 +1100

    chatInput UI && chatContext

---
+++ app/api/message/route.ts
@@ -0,0 +1,16 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+	//endpoint for asking a question to a PDF file
+
+	const body = await req.json();
+
+	const { getUser } = getKindeServerSession();
+	const user = getUser();
+
+	const { id: userId } = user;
+    if(!userId) return new Response('Unauthorized', {status:401})
+
+    
+};
