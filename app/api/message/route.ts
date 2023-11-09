70aeeda68767fb93622801576b47f0085faeb1d5
ahoang2502 <athoang252@gmail.com>
2023-11-09 15:10:48 +1100

    streaming API response in real-time

--- app/api/message/route.ts
+++ app/api/message/route.ts
@@ -1,5 +1,13 @@
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
+import { db } from "@/lib/db";
+import { getPineconeClient } from "@/lib/pinecone";
+import { openai } from "@/lib/openai";
 
 export const POST = async (req: NextRequest) => {
 	//endpoint for asking a question to a PDF file
@@ -10,7 +18,101 @@ export const POST = async (req: NextRequest) => {
 	const user = getUser();
 
 	const { id: userId } = user;
-    if(!userId) return new Response('Unauthorized', {status:401})
+	if (!userId) return new Response("Unauthorized", { status: 401 });
 
-    
+	const { fileId, message } = SendMessageValidator.parse(body);
+
+	const file = await db.file.findFirst({
+		where: {
+			id: fileId,
+			userId,
+		},
+	});
+
+	if (!file) return new Response("Not found.", { status: 404 });
+
+	await db.message.create({
+		data: {
+			text: message,
+			isUserMessage: true,
+			userId,
+			fileId,
+		},
+	});
+
+	// vectorize message
+	const embeddings = new OpenAIEmbeddings({
+		openAIApiKey: process.env.OPEN_API_KEY,
+	});
+
+	// Initialize the Pinecone vector store
+	const pinecone = await getPineconeClient();
+	const pineconeIndex = pinecone.Index("pdf-ai");
+
+	const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+		pineconeIndex,
+	});
+
+	try {
+		// Search for similar messages using the fileId as context
+		const results = await vectorStore.similaritySearch(message, 4);
+
+		const prevMessages = await db.message.findMany({
+			where: {
+				fileId,
+			},
+			orderBy: { createdAt: "desc" },
+			take: 6,
+		});
+
+		const formattedPrevMessages = prevMessages.map((msg) => ({
+			role: msg.isUserMessage ? ("user" as const) : ("assistant" as const),
+			content: msg.text,
+		}));
+
+		const response = await openai.chat.completions.create({
+			model: "gpt-3.5-turbo",
+			temperature: 0,
+			stream: true,
+			messages: [
+				{
+					role: "system",
+					content:
+						"Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.",
+				},
+				{
+					role: "user",
+					content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+  \n----------------\n
+
+  PREVIOUS CONVERSATION:
+  ${formattedPrevMessages.map((message) => {
+		if (message.role === "user") return `User: ${message.content}\n`;
+		return `Assistant: ${message.content}\n`;
+	})}
+
+  \n----------------\n
+
+  CONTEXT:
+  ${results.map((r) => r.pageContent).join("\n\n")}
+
+  USER INPUT: ${message}`,
+				},
+			],
+		});
+
+		const stream = OpenAIStream(response, {
+			async onCompletion(completion) {
+				await db.message.create({
+					data: { text: completion, isUserMessage: false, fileId, userId },
+				});
+			},
+		});
+
+		return new StreamingTextResponse(stream);
+	} catch (error) {
+		console.log("Error searching for similar messages: ", error);
+		return new Response("InternalServerError", { status: 500 });
+	}
 };
--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,6 +1,11 @@
-import { db } from "@/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+
+import { db } from "@/lib/db";
+import { getPineconeClient } from "@/lib/pinecone";
 
 const f = createUploadthing();
 
@@ -26,6 +31,57 @@ export const ourFileRouter = {
 					uploadStatus: "PROCESSING",
 				},
 			});
+
+			// Pinecone
+			try {
+				const response = await fetch(
+					`https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+				);
+				const blob = await response.blob();
+
+				// load pdf into memory
+				const loader = new PDFLoader(blob);
+
+				const pageLevelDocs = await loader.load();
+				const pagesAmt = pageLevelDocs.length;
+
+				const combinedData = pageLevelDocs.map((document) => {
+					return {
+						...document,
+						metadata: {
+							fileId: createdFile.id,
+						},
+						dataset: "pdf",
+					};
+				});
+
+				// vectorize and index entire document
+				const pinecone = await getPineconeClient();
+				const pineconeIndex = pinecone.Index("pdf-ai");
+
+				const embeddings = new OpenAIEmbeddings({
+					openAIApiKey: process.env.OPEN_API_KEY,
+				});
+
+				await PineconeStore.fromDocuments(combinedData, embeddings, {
+					pineconeIndex,
+				});
+
+				await db.file.update({
+					data: { uploadStatus: "SUCCESS" },
+					where: {
+						id: createdFile.id,
+					},
+				});
+			} catch (error) {
+				console.log("[pdfUploader]", error);
+				await db.file.update({
+					data: { uploadStatus: "FAILED" },
+					where: {
+						id: createdFile.id,
+					},
+				});
+			}
 		}),
 } satisfies FileRouter;
 
